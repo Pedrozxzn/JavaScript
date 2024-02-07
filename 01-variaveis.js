bb381d119fea7027c8b7a1ac6cf351fd6c1afa7a
Pedro <silvaelima557@gmail.com>
2024-02-07 11:26:05 -0300

    javascript-basico

---
+++ 01-variaveis.js
@@ -0,0 +1,11 @@
+//Criando váriaveis com "var" (var nome váriavel) = <valor>;)
+
+var nome = "Pedro";
+var idade = 15;
+
+console.log("Meu nome é", nome, "e tenho", idade, "anos");
+console.log("Agora fiz aníversario! Tenho", idade + 1);
+
+//aumentando autonaticamente +1(autoincremento)
+idade++; //idade = idade + 1
+console.log("Agora fiz aníversario! Tenho", idade, "anos!");
---
+++ 02-aritimetico.js
@@ -0,0 +1,25 @@
+//utilizando operadores aritimeticos com JavaScript
+
+// console.log("A soma entre 8 e 7 é:", 8 + 7);
+
+// //efetuar a soma de dois numeros informados por mim (var nome variavel = 12)
+// var x = 25;
+// var y = 26;
+// console.log("a soma entre", x, "e", y, "é:", x + y);
+
+// var soma = x + y;
+// var subitracao = x - y;
+// var multiplicacao = x * y;
+// var divisao = x / y;
+
+// console.log("Soma = ", soma);
+// console.log("Subitracao = ", subitracao);
+// console.log("Multiplicacao = ", multiplicacao);
+// console.log("Divisao = ", divisao);
+
+//regras para nomear identificadores (var, const ou let)
+//podem começar com A-Z, a-z _ ou $
+var MinhaVariavel = 10;
+var outravariavel = 20;
+const _minhavariavel = 3.1514;
+var $minhaVariavel = 11;
