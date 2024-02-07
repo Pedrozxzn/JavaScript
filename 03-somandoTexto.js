45dbcdd89e91aa2da7488b8d94752a1929f72afd
Pedro <silvaelima557@gmail.com>
2024-02-07 17:07:14 -0300

    adicinado atividade

---
+++ 03-somandoTexto.js
@@ -0,0 +1,10 @@
+var nome = "Pedro";
+var boasVindas = "Olá " + nome + ", seja bem vindo!";
+
+console.log(boasVindas);
+
+var texto1 = "Olá";
+var texto2 = "Bom Dia";
+
+console.log(texto1 + texto2);
+console.log(texto1 + ", " + texto2);
