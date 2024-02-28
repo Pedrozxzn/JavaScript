1fda9100189c60121bfe2e591fb1ff8cf3147154
Pedro <silvaelima557@gmail.com>
2024-02-28 10:07:23 -0300

    atividade 05

--- atividade05.js
+++ atividade05.js
@@ -4,10 +4,8 @@ function idade(ano) {
   return year - ano;
 }
 console.log(`Sua idade é ${idade(2008)}`);
-console.log("");
 console.log("--------------------------------------------------------");
-console.log("");
 function viagem(combustivel) {
   return combustivel * 12;
 }
-console.log(`Você poderá andar: ${viagem(55)}`);
+console.log(`Você poderá andar: ${viagem(55)}km`);
