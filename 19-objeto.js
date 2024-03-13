4f9103db34336382f517acb0ce8aab9505bf3229
Pedro <silvaelima557@gmail.com>
2024-03-13 10:23:18 -0300

    objetos

--- 19-objeto.js
+++ 19-objeto.js
@@ -1,144 +1,174 @@
-// //definindo uma classe pessoa
-// class Pessoa {
-//   constructor(pNome, pIdade) {
-//     this.nome = pNome;
-//     this.idade = pIdade;
-//   }
-//   apresentaSe() {
-//     console.log(`Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos`);
-//   }
-//   envelhecer() {
-//     this.idade++;
-//   }
-// }
+// // //definindo uma classe pessoa
+// // class Pessoa {
+// //   constructor(pNome, pIdade) {
+// //     this.nome = pNome;
+// //     this.idade = pIdade;
+// //   }
+// //   apresentaSe() {
+// //     console.log(`Olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos`);
+// //   }
+// //   envelhecer() {
+// //     this.idade++;
+// //   }
+// // }
 
-// //criando onjetos do 'tipo' pessoa
-// var p1 = new Pessoa("Dienifer", 52);
-// var p2 = new Pessoa("Taldo Roberto", 18);
+// // //criando onjetos do 'tipo' pessoa
+// // var p1 = new Pessoa("Dienifer", 52);
+// // var p2 = new Pessoa("Taldo Roberto", 18);
 
-// p1.apresentaSe();
-// p2.apresentaSe();
+// // p1.apresentaSe();
+// // p2.apresentaSe();
 
-// p1.envelhecer();
-// p1.apresentaSe();
+// // p1.envelhecer();
+// // p1.apresentaSe();
 
-// p2.envelhecer();
-// p2.apresentaSe();
-// console.log(
-//   "============================Abstração============================"
-// );
-// class Pet {
-//   constructor(nome, idade, tipo) {
-//     this.nome = nome;
-//     this.idade = idade;
-//     this.tipo = tipo;
-//   }
-//   alimentar() {
-//     console.log(`${this.nome} foi alimentado!`);
-//   }
-// }
-// const meuPet = new Pet("ben", 2, "Cachorro");
-// console.log(
-//   `Meu pet é um ${meuPet.tipo}, chamado ${meuPet.nome} e tem ${meuPet.idade} anos`
-// );
+// // p2.envelhecer();
+// // p2.apresentaSe();
+// // console.log(
+// //   "============================Abstração============================"
+// // );
+// // class Pet {
+// //   constructor(nome, idade, tipo) {
+// //     this.nome = nome;
+// //     this.idade = idade;
+// //     this.tipo = tipo;
+// //   }
+// //   alimentar() {
+// //     console.log(`${this.nome} foi alimentado!`);
+// //   }
+// // }
+// // const meuPet = new Pet("ben", 2, "Cachorro");
+// // console.log(
+// //   `Meu pet é um ${meuPet.tipo}, chamado ${meuPet.nome} e tem ${meuPet.idade} anos`
+// // );
 
-// meuPet.alimentar();
-// console.log("============================Herança=============================");
+// // meuPet.alimentar();
+// // console.log("============================Herança=============================");
+// // class Animal {
+// //   constructor(nome, idade, tipoAlimentacao) {
+// //     this.nome = nome;
+// //     this.idade = idade;
+// //     this.tipoAlimento = tipoAlimentacao;
+// //   }
+// //   alimentar() {
+// //     console.log(`O animal ${this.nome} foi alimentado`);
+// //   }
+// //   emitirSom() {
+// //     console.log(`Barulho padrão de animal`);
+// //   }
+// // }
+// // class Catiolo extends Animal {
+// //   alimentar() {
+// //     console.log(`O catiolo ${this.nome} foi alimentado`);
+// //   }
+// //   emitirSom() {
+// //     console.log(`AU AU`);
+// //   }
+// // }
+// // const pet = new Animal("Adalberto", 10, "Comida especial");
+// // const meuCachorro = new Catiolo("ben", 3, "Ração");
+
+// // pet.alimentar();
+// // pet.emitirSom();
+
+// // console.log(" ");
+
+// // meuCachorro.alimentar();
+// // meuCachorro.emitirSom();
+// // console.log("========================Encapsulamento==========================");
+// // class Petzinho {
+// //   constructor(nome, idade, tipo) {
+// //     this.nome = nome;
+// //     this.idade = idade;
+// //     this.tipo = tipo;
+// //   }
+// //   getNome() {
+// //     return this.nome;
+// //   }
+// //   setNome(novoNome) {
+// //     this.nome = novoNome;
+// //   }
+// // }
+// // const novoPetzinho = new Petzinho("Bob", 3, "Cachorro");
+
+// // console.log(novoPetzinho.getNome());
+// // novoPetzinho.setNome("Alfredo");
+// // console.log(novoPetzinho.getNome());
+// console.log("=======================Polimorfismo====================");
 // class Animal {
-//   constructor(nome, idade, tipoAlimentacao) {
+//   constructor(nome) {
 //     this.nome = nome;
-//     this.idade = idade;
-//     this.tipoAlimento = tipoAlimentacao;
 //   }
-//   alimentar() {
-//     console.log(`O animal ${this.nome} foi alimentado`);
-//   }
-//   emitirSom() {
-//     console.log(`Barulho padrão de animal`);
+
+//   fazerBarulho() {
+//     console.log("O animal faz um barulho genérico.");
 //   }
 // }
-// class Catiolo extends Animal {
-//   alimentar() {
-//     console.log(`O catiolo ${this.nome} foi alimentado`);
-//   }
-//   emitirSom() {
-//     console.log(`AU AU`);
-//   }
-// }
-// const pet = new Animal("Adalberto", 10, "Comida especial");
-// const meuCachorro = new Catiolo("ben", 3, "Ração");
 
-// pet.alimentar();
-// pet.emitirSom();
-
-// console.log(" ");
-
-// meuCachorro.alimentar();
-// meuCachorro.emitirSom();
-// console.log("========================Encapsulamento==========================");
-// class Petzinho {
-//   constructor(nome, idade, tipo) {
-//     this.nome = nome;
-//     this.idade = idade;
-//     this.tipo = tipo;
+// class Cachorro extends Animal {
+//   constructor(nome) {
+//     super(nome);
 //   }
-//   getNome() {
-//     return this.nome;
-//   }
-//   setNome(novoNome) {
-//     this.nome = novoNome;
+
+//   fazerBarulho() {
+//     console.log("O cachorro late!");
 //   }
 // }
-// const novoPetzinho = new Petzinho("Bob", 3, "Cachorro");
 
-// console.log(novoPetzinho.getNome());
-// novoPetzinho.setNome("Alfredo");
-// console.log(novoPetzinho.getNome());
-console.log('=======================Polimorfismo====================')
-class Animal {
-  constructor(nome) {
-    this.nome = nome;
+// class Gato extends Animal {
+//   constructor(nome) {
+//     super(nome);
+//   }
+
+//   fazerBarulho() {
+//     console.log("O gato mia!");
+//   }
+// }
+
+// // Exemplo de uso
+// const animais = [
+//   new Cachorro("Rex"),
+//   new Gato("Mia"),
+//   new Animal("Bob"), // Animal genérico
+// ];
+
+// for (const animal of animais) {
+//   animal.fazerBarulho();
+// }
+console.log(
+  "================================Exemplos==================================="
+);
+//criando uma classe retangulo
+class Retangulo {
+  constructor(comprimento, largura) {
+    this.comprimento = comprimento;
+    this.largura = largura;
   }
-
-
-  fazerBarulho() {
-    console.log("O animal faz um barulho genérico.");
+  obterArea() {
+    return this.comprimento * this.largura;
+  }
+  obterPerimetro() {
+    return this.comprimento * 2 + this.largura * 2;
+  }
+  setComprimento(novoComprimento) {
+    this.comprimento = novoComprimento;
+  }
+  setLargura(novaLargura) {
+    this.largura = novaLargura;
   }
 }
+//criando um objeto do "tipo" retangulo
+const ret1 = new Retangulo(10, 15);
 
-
-class Cachorro extends Animal {
-  constructor(nome) {
-    super(nome);
-  }
-
-
-  fazerBarulho() {
-    console.log("O cachorro late!");
-  }
+function displayRet(retangulo) {
+  console.table(retangulo);
+  console.log(`comprimento = ${retangulo.comprimento}`);
+  console.log(`largura = ${retangulo.largura}`);
+  console.log(`área = ${retangulo.obterArea()}`);
+  console.log(`perimetro = ${retangulo.obterPerimetro()}`);
 }
-
-
-class Gato extends Animal {
-  constructor(nome) {
-    super(nome);
-  }
-
-
-  fazerBarulho() {
-    console.log("O gato mia!");
-  }
-}
-
-
-// Exemplo de uso
-const animais = [
-  new Cachorro("Rex"),
-  new Gato("Mia"),
-  new Animal("Bob"), // Animal genérico
-];
-
-
-for (const animal of animais) {
-  animal.fazerBarulho();
-}
+displayRet(ret1);
+ret1.setComprimento(20);
+displayRet(ret1);
+ret1.setLargura(50);
+displayRet(ret1);
--- atividade08.js
+++ atividade08.js
@@ -1,31 +1,54 @@
-// Criar uma classe Aluno em JavaScript que represente um aluno em uma instituição de ensino. A classe deve receber informações do aluno, calcular sua idade e apresentar-se como métodos.
+// // Criar uma classe Aluno em JavaScript que represente um aluno em uma instituição de ensino. A classe deve receber informações do aluno, calcular sua idade e apresentar-se como métodos.
 
+// // Requisitos:
+// // A classe Aluno deve ter os seguintes atributos:
+// // ra: Número de Registro do Aluno (string)
+// // nome: Nome completo do aluno (string)
+// // dataNascimento: Data de nascimento do aluno (data)
+// // curso: Curso do aluno (string)
 
-// Requisitos:
-// A classe Aluno deve ter os seguintes atributos:
-// ra: Número de Registro do Aluno (string)
-// nome: Nome completo do aluno (string)
-// dataNascimento: Data de nascimento do aluno (data)
-// curso: Curso do aluno (string)
+// // A classe Aluno deve ter os seguintes métodos:
+// // calcularIdade(): Calcula e retorna a idade do aluno em anos.
+// // apresentar(): Exibe as informações do aluno no formato:
+// class Aluno {
+//     constructor(registroDeAluno, nome, nascimento, curso){
+//         this.registroDeAluno = registroDeAluno
+//         this.nome = nome
+//         this.nascimento = nascimento
+//         this.curso = curso
+// }
+// caucularIdade(){
+//    return 2024 - this.nascimento
+// }
+// apresentar(){
+//     console.log(`O nome do aluno é ${this.nome}, seu Registro de Aluno é ${this.registroDeAluno}, ele nasceu em ${this.nascimento}, tem ${this.caucularIdade()} anos , e faz parte do curso ${this.curso} `)
+// }
+// }
+// const aluno = new Aluno("9496", "Clodoaldo", 2008, "TI( Desenvolvimento de Sistemas ) no Senai de jau" )
+// aluno.caucularIdade()
+// aluno.apresentar()
 
-
-// A classe Aluno deve ter os seguintes métodos:
-// calcularIdade(): Calcula e retorna a idade do aluno em anos.
-// apresentar(): Exibe as informações do aluno no formato:
-class Aluno {
-    constructor(registroDeAluno, nome, nascimento, curso){
-        this.registroDeAluno = registroDeAluno
-        this.nome = nome
-        this.nascimento = nascimento
-        this.curso = curso
+//crie e urilize uma classe "sorvete" contendo as propriedades: sabor, preço, e tamanho ( pequeno | médio | grande )
+class Sorvete {
+  constructor(sabor, tamanho, preco) {
+    this.sabor = sabor;
+    this.preco = preco;
+    this.tamanho = tamanho;
+  }
+  saborSorvete() {
+    console.log(
+      `Olá meu nome é Alberto e eu irei pegar um sorvete de sabor ${this.sabor}, de tamanho ${this.tamanho} que custa ${this.preco}`
+    );
+  }
+  alterarPreco(precoNovo) {
+    this.preco = precoNovo;
+  }
 }
-caucularIdade(){
-   return 2024 - this.nascimento
-}
-apresentar(){
-    console.log(`O nome do aluno é ${this.nome}, seu Registro de Aluno é ${this.registroDeAluno}, ele nasceu em ${this.nascimento}, tem ${this.caucularIdade()} anos , e faz parte do curso ${this.curso} `)
-}
-}
-const aluno = new Aluno("9496", "Clodoaldo", 2008, "TI( Desenvolvimento de Sistemas ) no Senai de jau" )
-aluno.caucularIdade()
-aluno.apresentar()
+var preco1 = new Sorvete("Morango", "G", "R$ 10,00");
+preco1.saborSorvete();
+var preco2 = new Sorvete("Chocolate", "P", "R$ 11,35");
+preco2.saborSorvete();
+var preco3 = new Sorvete("Melancia", "M", "R$ 9,90");
+preco3.saborSorvete();
+preco1.alterarPreco(`R$ 10,51`);
+preco1.saborSorvete();
